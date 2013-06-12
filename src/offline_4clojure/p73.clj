2ac9dd556ee997e180240f018820e3b0a88247bf
Dave Aitken <dave.aitken@gmail.com>
2013-06-12 18:03:57 +0100

    Dodgy tick tack toe checker

--- src/offline_4clojure/p73.clj
+++ src/offline_4clojure/p73.clj
@@ -5,31 +5,43 @@
 (ns offline-4clojure.p73
   (:use clojure.test))
 
-(def __
-;; your solution here
-)
+(def __ 
+  (fn [board]
+    (letfn [
+            (has-winner [coll] (some #(if (not (or (nil? %) (= :e %))) % false) coll))
+            (row-winner [r] (if (apply = r) (first r) nil))
+            (transpose [b] (apply mapv vector b))
+            (winner-by-row [b] (has-winner (map row-winner b)))
+            (winner-by-col [b] (winner-by-row (transpose b)))
+            (winner-by-diag [b] (has-winner (list (if (or (= (get (get b 0) 0) (get (get b 1) 1) (get (get b 2) 2))
+                                         (= (get (get b 0) 2) (get (get b 1) 1) (get (get b 2) 0)))
+                                  (get (get b 1) 1) nil))))]
+      (some identity [(winner-by-row board) (winner-by-col board) (winner-by-diag board)])))
+  )
 
 (defn -main []
   (are [x] x
-(= nil (__ [[:e :e :e]
-            [:e :e :e]
-            [:e :e :e]]))
-(= :x (__ [[:x :e :o]
-           [:x :e :e]
-           [:x :e :o]]))
-(= :o (__ [[:e :x :e]
-           [:o :o :o]
-           [:x :e :x]]))
-(= nil (__ [[:x :e :o]
-            [:x :x :e]
-            [:o :x :o]]))
-(= :x (__ [[:x :e :e]
-           [:o :x :e]
-           [:o :e :x]]))
-(= :o (__ [[:x :e :o]
-           [:x :o :e]
-           [:o :e :x]]))
-(= nil (__ [[:x :o :x]
-            [:x :o :x]
-            [:o :x :o]]))
-))
+       (= nil (__ [[:e :e :e]
+                   [:e :e :e]
+                   [:e :e :e]]))
+       (= :x (__ [[:x :e :o]
+                  [:x :e :e]
+                  [:x :e :o]]))
+       (= :o (__ [[:e :x :e]
+                  [:o :o :o]
+                  [:x :e :x]]))
+       (= nil (__ [[:x :e :o]
+                   [:x :x :e]
+                   [:o :x :o]]))
+       (= :x (__ [[:x :e :e]
+                  [:o :x :e]
+                  [:o :e :x]]))
+       (= :o (__ [[:x :e :o]
+                  [:x :o :e]
+                  [:o :e :x]]))
+       (= nil (__ [[:x :o :x]
+                   [:x :o :x]
+                   [:o :x :o]]))
+       ))
+
+(-main)
