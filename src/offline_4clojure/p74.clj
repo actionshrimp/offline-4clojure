1f18ee652588b8724fb8217de635749bc6c690a2
Dave Aitken <dave.aitken@gmail.com>
2013-06-13 17:25:55 +0100

    Finished 74

--- src/offline_4clojure/p74.clj
+++ src/offline_4clojure/p74.clj
@@ -6,11 +6,22 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
-)
+  (fn [s] 
+    (let [isSquare (fn [x]
+                     (let [n (Integer/parseInt x)
+                           sqrt (Math/sqrt n)
+                           floored (int (Math/floor sqrt))]
+                       (= n (* floored floored))))]
+    (->> s
+         (re-seq #"\d+")
+         (filter isSquare)
+         (clojure.string/join ","))))
+  )
 
 (defn -main []
   (are [x] x
-(= (__ "4,5,6,7,8,9") "4,9")
-(= (__ "15,16,25,36,37") "16,25,36")
-))
+       (= (__ "4,5,6,7,8,9") "4,9")
+       (= (__ "15,16,25,36,37") "16,25,36")
+       ))
+
+(-main)
