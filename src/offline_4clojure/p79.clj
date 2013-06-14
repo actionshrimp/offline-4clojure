86c766565c447c767efb61836668ae1b5eae861b
Dave Aitken <dave.aitken@gmail.com>
2013-06-14 14:22:24 +0100

    Finished 79 with a behemoth solution

--- src/offline_4clojure/p79.clj
+++ src/offline_4clojure/p79.clj
@@ -5,9 +5,30 @@
 (ns offline-4clojure.p79
   (:use clojure.test))
 
-(def __
-;; your solution here
-)
+(def __ 
+  (fn [t]
+    (letfn [(all-paths-for [num-rows]
+             (map reverse (loop [paths [()] current-row 1]
+               (if (> current-row num-rows) 
+                 paths
+                 (recur
+                   (mapcat (fn [i] (map #(conj % i) paths)) (range current-row))
+                   (inc current-row))))))
+            (valid-path? [p] 
+              (get (reduce #(if (or (= (get % :last) %2) (= (+ 1 (get % :last)) %2)) 
+                              {:valid? (get % :valid?) :last %2}
+                              {:valid? false :last %2}) 
+                           {:valid? true :last (first p)}
+                           (rest p)) :valid?))
+            (valid-paths-for [num-rows] 
+              (filter valid-path? (all-paths-for num-rows)))
+            (cost-of-path [p]
+              (apply + (map #(get %1 %2) t p)))]
+      (->> (valid-paths-for (count t))
+        (map cost-of-path)
+        (sort)
+        (first))))
+  )
 
 (defn -main []
   (are [x] x
@@ -22,3 +43,5 @@
         [4 6 6 7 8]
        [5 7 3 5 1 4]))) ; 3->4->3->2->7->1
 ))
+
+(-main)
