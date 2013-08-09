ad690e82426ca59221a66fa9d0fa80bb1279bfb8
Dave Aitken <dave.aitken@gmail.com>
2013-08-09 18:03:10 +0100

    WIP 94

--- src/offline_4clojure/p94.clj
+++ src/offline_4clojure/p94.clj
@@ -6,7 +6,36 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
+  (fn gol-step [board]
+    (letfn [(cell [x y] (get (board y) x))
+            (out-of-bounds? [x y] (or (< x 0) (< y 0)))
+            (neighbours [x y] 
+              (filter #(not (apply out-of-bounds? %))
+                      [[(- x 1) (- y 1)] [x (- y 1)] [(+ x 1) (- y 1)]
+                       [(- x 1) y]       ,,,,,,,,,,, [(+ x 1) y]
+                       [(- x 1) (+ y 1)] [x (+ y 1)] [(+ x 1) (+ y 1)]]))
+            (step-cell [x y]
+              (prn "zomg" x y)
+              (let [neighbour-cells (map #(apply cell %) (neighbours x y))
+                    alive-neighbours (count (filter (partial = \#) neighbour-cells))]
+                (prn "here")
+                (prn (neighbours x y))
+                (prn neighbour-cells)
+                (prn alive-neighbours)
+                (prn (cell x y))
+                (if (= \# (cell x y))
+                  (cond
+                    (< alive-neighbours 2) \space
+                    (< alive-neighbours 4) \#
+                    :else \space)
+                  (if (= alive-neighbours 3) \# \space))))]
+      (map-indexed 
+        (fn [j row]
+          (prn "row: " j)
+          (map-indexed 
+            (fn [i _]
+              (prn "col: " i)
+              (step-cell i j)) row)) board)))
 )
 
 (defn -main []
