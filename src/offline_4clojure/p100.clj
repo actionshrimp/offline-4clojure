afd82c2feab8edcbe78e625ae6a6ef4d2717d119
Dave Aitken <dave.aitken@gmail.com>
2013-08-28 12:01:22 +0100

    Finished 100

--- src/offline_4clojure/p100.clj
+++ src/offline_4clojure/p100.clj
@@ -2,11 +2,22 @@
 ; Write a function which calculates the <a href="http://en.wikipedia.org/wiki/Least_common_multiple">least common multiple</a>.  Your function should accept a variable number of positive integers or ratios. 
 ; tags - math
 ; restricted - 
+
+;LCM of fractions = LCM of numerators / HCF of denominators
+
 (ns offline-4clojure.p100
   (:use clojure.test))
 
 (def __
-;; your solution here
+  (fn [& xs]
+    (let [gcd (fn [a b] (if (= b 0) a (recur b (mod a b))))
+          lcm (fn [a b] (/ (* a b) (gcd a b)))
+          numer #(if (integer? %) % (numerator %))
+          denom #(if (integer? %) 1 (denominator %))
+          f (fn [x y] (/ 
+                        (lcm (numer x) (numer y))
+                        (gcd (denom x) (denom y))))]
+    (reduce f xs)))
 )
 
 (defn -main []
