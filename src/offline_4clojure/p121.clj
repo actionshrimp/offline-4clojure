2a6d0130e9ced41a6bd7a664d2c3c9c1b25e930c
Dave Aitken <dave.aitken@gmail.com>
2013-11-29 14:25:57 +0000

    WIP 121

--- src/offline_4clojure/p121.clj
+++ src/offline_4clojure/p121.clj
@@ -12,23 +12,28 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
+  (fn [expr]
+    (fn [values]
+      (let [replace-syms (fn f [x] 
+                           (if (sequential? x)
+                             (map f x)
+                             (if (values x) (values x) x)))]
+        (eval (map replace-syms expr)))))
 )
 
 (defn -main []
-  (are [x] x
-(= 2 ((__ '(/ a b))
-      '{b 8 a 16}))
-(= 8 ((__ '(+ a b 2))
-      '{a 2 b 4}))
-(= [6 0 -4]
-     (map (__ '(* (+ 2 a)
-  	              (- 10 b)))
-	        '[{a 1 b 8}
-	          {b 5 a -2}
-	          {a 2 b 11}]))
-(= 1 ((__ '(/ (+ x 2)
-              (* 3 (+ y 1))))
-      '{x 4 y 1}))
-
-))
+  (are [value] value
+       (= 2 ((__ '(/ a b))
+             '{b 8 a 16}))
+       (= 8 ((__ '(+ a b 2))
+             '{a 2 b 4}))
+       (= [6 0 -4]
+          (map (__ '(* (+ 2 a)
+                       (- 10 b)))
+               '[{a 1 b 8}
+                 {b 5 a -2}
+                 {a 2 b 11}]))
+       (= 1 ((__ '(/ (+ x 2)
+                     (* 3 (+ y 1))))
+             '{x 4 y 1}))
+       ))
